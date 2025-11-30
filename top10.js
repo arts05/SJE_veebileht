fc63336214270ec2f8492dbdc345effb5dfb87a8
joepeify <poldmajoonas@gmail.com>
2025-11-30 22:17:30 +0200

    Go to top nupp

---
+++ top10.js
@@ -0,0 +1,26 @@
+// Üleskerimis nupp
+const scrollTop = function () {
+};
+scrollTop();
+
+const scrollBtn = document.createElement("button");
+scrollBtn.innerHTML = "Ülesse&nbsp;&uarr;";
+scrollBtn.setAttribute("id", "scroll-btn");
+document.body.appendChild(scrollBtn);
+
+const scrollBtnDisplay = function () {
+  window.scrollY > window.innerHeight
+    ? scrollBtn.classList.add("show")
+    : scrollBtn.classList.remove("show");
+};
+window.addEventListener("scroll", scrollBtnDisplay);
+
+const scrollWindow = function () {  
+  if (window.scrollY != 0) {
+    setTimeout(function () {
+      window.scrollTo(0, window.scrollY - 50);
+      scrollWindow();
+    }, 10);
+  }
+};
+scrollBtn.addEventListener("click", scrollWindow);
